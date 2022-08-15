83e14379b4da7897d3b271ff72f73237b899dcfa
Namget92 <timtegman@gmail.com>
2022-08-15 10:48:41 +0200

    second

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const PORT = process.env.PORT || 4000;
-const aws = require("aws-sdk");
-require("dotenv").config();
-
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET,
-});
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Hello ${process.env.DUMMY_TEXT}`);
+  res.send("Heja Hammarby!");
 });
 
 app.listen(PORT, () => {
